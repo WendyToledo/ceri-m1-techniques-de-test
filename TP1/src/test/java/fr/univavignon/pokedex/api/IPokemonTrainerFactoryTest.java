ec38922c0dd7c68e6bbf3a7643ea42ede2e8fce1
Wendy Toledo <144907324+WendyToledo@users.noreply.github.com>
2024-11-15 12:51:30 +0100

    Add files via upload

--- TP1/src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ TP1/src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -9,17 +9,18 @@ import org.junit.jupiter.api.BeforeEach;
 public class IPokemonTrainerFactoryTest {
 	
 	private IPokemonTrainerFactory factory;
+	
 	private IPokedexFactory pokeFacto;
 	private IPokedex poke;
 	
 	@BeforeEach
 	public void setup() {
 		
-		factory = mock(IPokemonTrainerFactory.class);
+		factory = new PokemonTrainerFactory();
 		
 		pokeFacto = mock(IPokedexFactory.class);
-		
 		poke = mock(IPokedex.class);
+		
 		when(pokeFacto.createPokedex(any(), any())).thenReturn(poke);
 	}
 	
@@ -28,17 +29,17 @@ public class IPokemonTrainerFactoryTest {
 		String name = "Blue";
 		Team team = Team.INSTINCT;
 		
-		PokemonTrainer trainer = new PokemonTrainer(name, team, poke);
+		PokemonTrainer trainer = factory.createTrainer(name, team, pokeFacto);
 		when(factory.createTrainer(name, team, pokeFacto)).thenReturn(trainer);	
 		
 		PokemonTrainer createdTrainer = factory.createTrainer(name, team, pokeFacto);
 		
 		assertNotNull(createdTrainer);
-		assertEquals("Blue", createdTrainer.getName());
-		assertEquals(Team.INSTINCT, createdTrainer.getTeam());
+		assertEquals(name, createdTrainer.getName());
+		assertEquals(team, createdTrainer.getTeam());
 		assertEquals(poke, createdTrainer.getPokedex());
 		
-		
+		verify(pokeFacto).createPokedex(any(), any());
 	}
 
 }
